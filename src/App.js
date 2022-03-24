b9777232488d22935f18253de07a11886d6fa28a
felipekako <felipekakosampaio@gmail>
2022-03-24 11:32:13 -0300

    colocando barra

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+const appi = {
+  kay: "b4dffd7b70953ee9b9d3fa84516ce89c",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <main>
+       <div className="search-box">
+         <input
+         type="text"
+          classsName="search-bar"
+          placeholder="digite a cidade"
+         
+         />
+       </div>
+     </main>
     </div>
   );
 }
